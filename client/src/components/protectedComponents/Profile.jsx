42009c1b6f661946c301d28f103ba1fbecdfbe79
Galsha33 <galsha2297@gmail.com>
2023-02-13 16:32:08 +0200

    first

---
+++ client/src/components/protectedComponents/Profile.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "../../styles/profile.css";
+import AddingEvent from "./AddingEvent";
+import AddingUser from "./AddingUser";
+import SideBarProfile from "./sideBarProfile";
+import EditingUser from "./EditingUser";
+import Chat from "./Chat";
+import ChatRoom from "./ChatRoom";
+
+function Profile() {
+  return (
+    <React.Fragment>
+      <body className="body">
+        <div className="sidebar">
+          <SideBarProfile />
+        </div>
+        <div className="container">
+          <Routes>
+            <Route path="AddingUser" element={<AddingUser />} />
+            <Route path="AddingEvent" element={<AddingEvent />} />
+            <Route path="EditingUser" element={<EditingUser />} />
+            <Route path="ChatRoom" element={<ChatRoom />} />
+            <Route path="Chat" element={<Chat />} />
+          </Routes>
+        </div>
+      </body>
+    </React.Fragment>
+  );
+}
+
+export default Profile;
